fb5e430f41cbd7010c346cab69f54e3abc7a6b06
Cristian Adán <cristian@salteadorneo.dev>
2025-08-17 14:44:51 +0200

    Init

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Eventos de mesa';
+export const SITE_DESCRIPTION = 'Eventos de mesa es una plataforma para organizar eventos de juegos de rol, wargames y otros juegos de mesa.';
---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+    // Load Markdown and MDX files in the `src/content/blog/` directory.
+    loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+    // Type-check frontmatter using a schema
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            description: z.string(),
+            start: z.coerce.date(),
+            end: z.coerce.date().optional(),
+            updatedDate: z.coerce.date().optional(),
+            heroImage: image().optional(),
+            location: z.string().optional(),
+            // Recurring event properties
+            daysOfWeek: z.array(z.number()).optional(),
+            startTime: z.string().optional(),
+            endTime: z.string().optional(),
+            startRecur: z.coerce.date().optional(),
+            endRecur: z.coerce.date().optional(),
+        }),
+});
+
+export const collections = { blog };
